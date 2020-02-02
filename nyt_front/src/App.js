192922748fbf8537fbcddbcb2a86d344e0dcebbb
rishav <rishav.21m@gmail.com>
2020-02-03 00:05:15 +0530

    added frontend

---
+++ nyt_front/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component() {
+  constructor(props){
+    super(props);
+    this.state={
+
+    }
+  }
+
+  render(){
+    return (
+      <div>hello</div>
+    );    
+  }
+}
+
+export default App;
